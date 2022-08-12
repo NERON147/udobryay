864207e024a37ea8baae6cbc66a0c7ac4cbbdd9d
NERON147 <bezzubenkomaxim@gmail.com>
2022-08-13 02:27:15 +0300

    qq

--- js/script.js
+++ js/script.js
@@ -20,26 +20,36 @@ const swiper = new Swiper('.swiper', {
   ScrollReveal().reveal('.n3', {  distance: '60px', origin: 'left', delay: 500, easing: 'ease'});
   ScrollReveal().reveal('.n4', {  distance: '60px', origin: 'left', delay: 600, easing: 'ease'});
   ScrollReveal().reveal('.n5', {  distance: '60px', origin: 'left', delay: 700, easing: 'ease'});
+  ScrollReveal().reveal('.i', {  distance: '60px', origin: 'right', delay: 1000, easing: 'ease'});
+  ScrollReveal().reveal('.i1', {  distance: '60px', origin: 'right', delay: 1100, easing: 'ease'});
+  ScrollReveal().reveal('.i2', {  distance: '60px', origin: 'right', delay: 1200, easing: 'ease'});
+  ScrollReveal().reveal('.i3', {  distance: '60px', origin: 'right', delay: 1300, easing: 'ease'});
   ScrollReveal().reveal('.logo2', {  distance: '60px', origin: 'top',  rotate: {
     x: 200,
     y: 0,
     z: 100,
-}, delay: 500,  easing: 'ease' });
-  ScrollReveal().reveal('.img2', {  distance: '60px', origin: 'right',  delay: 300});
-  ScrollReveal().reveal('.form__buy3', {  distance: '60px', origin: 'left',  delay: 500 });
-  ScrollReveal().reveal('.buy3', {  distance: '60px', origin: 'right', delay: 1000,
+}, delay: 900,  easing: 'ease' });
+  ScrollReveal().reveal('.img2', {  distance: '60px', origin: 'right',  delay: 800, easing: 'ease'});
+  ScrollReveal().reveal('.form__buy3', {  distance: '60px', origin: 'left',  delay: 500 , easing: 'ease'});
+  ScrollReveal().reveal('.buy3', {  distance: '60px', origin: 'right', delay: 1000, easing: 'ease',
   rotate: {
     x: 100,
     y: 0,
     z: 100,
 }});
 ScrollReveal().reveal('.main__text2', {  distance: '60px', origin: 'bottom',  delay: 700, easing: 'ease' });
-ScrollReveal().reveal('.katalog2', {  distance: '60px', origin: 'left',delay: 400});
-ScrollReveal().reveal('.about__text2', {  distance: '60px', origin: 'right', delay: 400});
-ScrollReveal().reveal('.smotry2', {  distance: '60px', origin: 'top', delay: 800});
-ScrollReveal().reveal('.slider2', {  distance: '60px', origin: 'bottom', delay: 1100});
-ScrollReveal().reveal('.left__cont2', {  distance: '60px', origin: 'left', delay: 1100});
-ScrollReveal().reveal('.right__cont2', {  distance: '60px', origin: 'right', delay: 1400});
+ScrollReveal().reveal('.katalog2', {  distance: '60px', origin: 'left',delay: 900, easing: 'ease-in'});
+ScrollReveal().reveal('.about__text2', {  distance: '60px', origin: 'right', delay: 900, easing: 'ease-in'});
+ScrollReveal().reveal('.smotry2', {  distance: '60px', origin: 'top', delay: 800, easing: 'ease'});
+ScrollReveal().reveal('.slider2', {  distance: '60px', origin: 'bottom', delay: 1100, easing: 'ease'});
+ScrollReveal().reveal('.left__cont2', {  distance: '60px', origin: 'left', delay: 1100, easing: 'ease'});
+ScrollReveal().reveal('.right__cont2', {  distance: '60px', origin: 'right', delay: 1800, easing: 'ease'});
+ScrollReveal().reveal('.buy4', {  distance: '60px', origin: 'right', delay: 1900, easing: 'ease',
+rotate: {
+  x: 100,
+  y: 0,
+  z: 100,
+}});
 
-ScrollReveal().reveal('.form__buy4', {  distance: '60px', origin: 'bottom', delay: 400});
+ScrollReveal().reveal('.form__buy4', {  distance: '60px', origin: 'bottom', delay: 1400, easing: 'ease'});
 ScrollReveal().reveal('.logo--footer', {  distance: '60px', origin: 'bottom', delay: 400});
